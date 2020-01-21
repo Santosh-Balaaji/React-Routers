7b1f834ae5ad370d3946d275cfd32572cca027d2
Santosh Selvakumar <Santosh.Selvakumar@serko.com>
2020-01-22 10:35:31 +1300

    React_router added with HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-
-
-
-function App() {
+import React,{Component} from 'react';
+import NavBar from './Components/Navbar';
+import {BrowserRouter, Route} from 'react-router-dom'; //Router configuration -- wrap the component with the BrowserRouter tag and use the Route path to specify the route location
+import Contact from './Components/Contact';
+import About from './Components/About';
+import Home from './Components/Home';
+class App extends Component{
+  render(){
   return (
+    <BrowserRouter>
     <div className="App">
-      
+      <NavBar />
+      <Route path='/Contact' component={Contact}/>  
+      <Route exact path='/' component={Home}/>
+      <Route path='/About' component={About}/>
     </div>
+    </BrowserRouter>
   );
+  }
 }
 
 export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link, NavLink, withRouter} from 'react-router-dom'  // use the Link and NavLink so that the react-router fetches and renders each page without hitting the server everytime on a reload
+// WithRouter is the higher order component used to provide additional functionality in this case adding the history to props over here. 
+const NavBar =(props)=>{
+    console.log(props)
+    setTimeout(()=>{
+        props.history.push('/about') //props.history can be used only inside a single component and cannot be accessed here. Higher ORder components are used to make props use history properties over here.
+    },2000);
+    return (
+        <nav className="nav-wrapper read darken-3">
+        <div className="container">
+        <a className = "brand-logo">Poke'Times</a>
+        <ul className ="right">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+        </div>
+        </nav>
+    );
+}
+
+export default withRouter(NavBar)
